a7291e81fe16eafc914c1c10dd8bbb2fe330cd65
rafael isac <rafael.darrigo@gmail.com>
2025-11-21 12:50:17 -0300

    route_addTransacao_3

--- src/controllers/transacoes.ts
+++ src/controllers/transacoes.ts
@@ -1,14 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { bodyTransacao } from "../types/transacaoTypes";
+import { readFile, writeFile } from "fs/promises";
 
-export const addTransacao = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const body = bodyTransacao.parse(req.body)
+export const addTransacao = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const body = bodyTransacao.parse(req.body);
 
-        res.status(200).json({ add: true })
+    const fileContent = await readFile("./trans.txt", {
+      encoding: "utf-8",
+    });
 
-    }
-    catch (err: any) {
-        next(err);
-    }
+    const jsonContent = JSON.parse(fileContent);
+    jsonContent.push(body);
+
+    const jsonString = JSON.stringify(jsonContent);
+
+    await writeFile("./trans.txt", jsonString);
+
+    res.status(201).json({});
+  } catch (err: any) {
+    next(err);
+  }
 };
